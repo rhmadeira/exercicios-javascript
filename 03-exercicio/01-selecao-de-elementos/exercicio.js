b9679966c43c122d5d2b2c0ef3d5c965f632cd1a
Rafael Henrique Madeira Feitosa <98665149+rafaelhenriquemadeira@users.noreply.github.com>
2022-05-29 12:38:32 -0300

    Pasta de  exercício 03

---
+++ 03-exercicio/01-selecao-de-elementos/exercicio.js
@@ -0,0 +1,21 @@
+//Retorne no console todas as imagens do site
+
+const img = document.querySelectorAll('img')
+
+
+//Retorne no console apenas as imagens que  comecaram com a palavra imagem
+
+const imagensDosAnimais = document.querySelectorAll('img[src^="./img/imagem"]')
+
+//selecione todos os links internos
+
+const linksInternos = document.querySelectorAll('a[href^="#"]')
+
+//Selecione o primeiro h2 dentro de .animais-descricao
+
+const primeiroH2 = document.querySelector('.animais-descricao h2')
+
+//Selecione o ultimo p do site
+
+const paragrafos = document.querySelectorAll('p')
+const ultimoP = paragrafos[--paragrafos.length]
